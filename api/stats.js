19b2cbf195836522779a3bfdb7dd9788b16ba410
KetXanh <169112526+KetXanh@users.noreply.github.com>
2025-11-11 23:16:45 +0700

    Hello1

--- api/stats.js
+++ api/stats.js
@@ -1,10 +1,24 @@
-import { clicks } from "./behance";
-
 export default function handler(req, res) {
   res.setHeader("Content-Type", "text/html");
 
+  // Demo logs vì serverless không chia sẻ memory
+  const clicks = [
+    {
+      time: "11/11/2025 23:00",
+      city: "Hanoi",
+      device: "desktop - Windows 10",
+      referrer: "Direct",
+    },
+    {
+      time: "11/11/2025 23:01",
+      city: "Ho Chi Minh",
+      device: "mobile - iOS 17",
+      referrer: "Direct",
+    },
+  ];
+
   let html = `
-    <h1>Behance Click Tracker (VN)</h1>
+    <h1>Behance Click Tracker (Demo)</h1>
     <p><b>Tổng: ${clicks.length} lượt bấm</b></p>
     <table border="1" cellpadding="5" cellspacing="0">
       <tr><th>Thời gian</th><th>Thành phố</th><th>Thiết bị</th><th>Nguồn</th></tr>
@@ -18,6 +32,5 @@ export default function handler(req, res) {
     });
 
   html += `</table><hr><a href="/">Quay lại</a>`;
-
   res.status(200).send(html);
 }
